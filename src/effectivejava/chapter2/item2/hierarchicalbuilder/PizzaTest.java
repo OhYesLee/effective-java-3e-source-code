3e3216c670794b78ec569cad39da06a1e454d242
Wegra Lee <wegra.lee@gmail.com>
2018-10-18 21:27:34 +0900

    한글화

--- src/effectivejava/chapter2/item2/hierarchicalbuilder/PizzaTest.java
+++ src/effectivejava/chapter2/item2/hierarchicalbuilder/PizzaTest.java
@@ -3,7 +3,7 @@ package effectivejava.chapter2.item2.hierarchicalbuilder;
 import static effectivejava.chapter2.item2.hierarchicalbuilder.Pizza.Topping.*;
 import static effectivejava.chapter2.item2.hierarchicalbuilder.NyPizza.Size.*;
 
-// Using the hierarchical builder (Page 16)
+// 계층적 빌더 사용 (21쪽)
 public class PizzaTest {
     public static void main(String[] args) {
         NyPizza pizza = new NyPizza.Builder(SMALL)
