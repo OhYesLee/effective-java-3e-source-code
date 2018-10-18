782e27787b15c25811679502945d41b560b2204f
Wegra Lee <wegra.lee@gmail.com>
2018-10-18 21:55:35 +0900

    한글화

--- src/effectivejava/chapter2/item8/Teenager.java
+++ src/effectivejava/chapter2/item8/Teenager.java
@@ -2,13 +2,14 @@ package effectivejava.chapter2.item8;
 
 import java.util.concurrent.TimeUnit;
 
-// Ill-behaved client of resource with cleaner safety-net (Page 33)
+// cleaner 안전망을 갖춘 자원을 제대로 활용하지 못하는 클라이언트 (45쪽)
 public class Teenager {
     public static void main(String[] args) {
         new Room(99);
         System.out.println("Peace out");
 
-        // Uncomment next line and retest behavior, but note that you MUST NOT depend on this behavior!
+        // 다음 줄의 주석을 해제한 후 동작을 다시 확인해보자.
+        // 단, 가비지 컬렉러를 강제로 호출하는 이런 방식에 의존해서는 절대 안 된다!
 //      System.gc();
     }
 }
