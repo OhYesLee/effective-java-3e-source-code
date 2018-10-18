6d31c8dd8b287f7f1a972db7df3570966872b7f5
Wegra Lee <wegra.lee@gmail.com>
2018-10-18 21:22:11 +0900

    한글화

--- src/effectivejava/chapter2/item2/hierarchicalbuilder/Pizza.java
+++ src/effectivejava/chapter2/item2/hierarchicalbuilder/Pizza.java
@@ -1,9 +1,10 @@
 package effectivejava.chapter2.item2.hierarchicalbuilder;
 import java.util.*;
 
-// Builder pattern for class hierarchies (Page 14)
+// 코드 2-4 계층적으로 설계된 클래스와 잘 어울리는 빌더 패턴 (19쪽)
 
-// Note that the underlying "simulated self-type" idiom  allows for arbitrary fluid hierarchies, not just builders
+// 참고: 여기서 사용한 '시뮬레이트한 셀프 타입(simulated self-type)' 관용구는
+// 빌더뿐 아니라 임의의 유동적인 계층구조를 허용한다.
 
 public abstract class Pizza {
     public enum Topping { HAM, MUSHROOM, ONION, PEPPER, SAUSAGE }
@@ -18,11 +19,12 @@ public abstract class Pizza {
 
         abstract Pizza build();
 
-        // Subclasses must override this method to return "this"
+        // 하위 클래스는 이 메서드를 재정의(overriding)하여
+        // "this"를 반환하도록 해야 한다.
         protected abstract T self();
     }
     
     Pizza(Builder<?> builder) {
-        toppings = builder.toppings.clone(); // See Item 50
+        toppings = builder.toppings.clone(); // 아이템 50 참조
     }
 }
