89cd739211dad6bdfc12b17d3c3ab8ca1cbdeb1a
Wegra Lee <wegra.lee@gmail.com>
2018-10-18 21:31:21 +0900

    한글화

--- src/effectivejava/chapter2/item3/field/Elvis.java
+++ src/effectivejava/chapter2/item3/field/Elvis.java
@@ -10,7 +10,7 @@ public class Elvis {
         System.out.println("Whoa baby, I'm outta here!");
     }
 
-    // 이 메서드는 보통 클래스 바깥(다른 클래스)에 있어야 한다!
+    // 이 메서드는 보통 클래스 바깥(다른 클래스)에 작성해야 한다!
     public static void main(String[] args) {
         Elvis elvis = Elvis.INSTANCE;
         elvis.leaveTheBuilding();
