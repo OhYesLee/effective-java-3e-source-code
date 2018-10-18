59f283196bca730d678376ef79c160cec8beb08f
Wegra Lee <wegra.lee@gmail.com>
2018-10-18 21:59:13 +0900

    한글화

--- src/effectivejava/chapter2/item9/trywithresources/TopLineWithDefault.java
+++ src/effectivejava/chapter2/item9/trywithresources/TopLineWithDefault.java
@@ -6,7 +6,7 @@ import java.io.FileReader;
 import java.io.IOException;
 
 public class TopLineWithDefault {
-    // try-with-resources with a catch clause  (Page 36)
+    // 코드 9-5 try-with-resources를 catch 절과 함께 쓰는 모습 (49쪽)
     static String firstLineOfFile(String path, String defaultVal) {
         try (BufferedReader br = new BufferedReader(
                 new FileReader(path))) {
