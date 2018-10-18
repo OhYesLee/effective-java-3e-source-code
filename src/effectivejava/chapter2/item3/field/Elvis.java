1fd4d18053108768b492ea1437a7ed4ec96f12f1
Wegra Lee <wegra.lee@gmail.com>
2018-10-18 21:29:40 +0900

    한글화

--- src/effectivejava/chapter2/item3/field/Elvis.java
+++ src/effectivejava/chapter2/item3/field/Elvis.java
@@ -1,6 +1,6 @@
 package effectivejava.chapter2.item3.field;
 
-// Singleton with public final field  (Page 17)
+// 코드 3-1 public static final 필드 방식의 싱글턴 (23쪽)
 public class Elvis {
     public static final Elvis INSTANCE = new Elvis();
 
@@ -10,7 +10,7 @@ public class Elvis {
         System.out.println("Whoa baby, I'm outta here!");
     }
 
-    // This code would normally appear outside the class!
+    // 이 메서드는 보통 클래스 바깥(다른 클래스)에 있어야 한다!
     public static void main(String[] args) {
         Elvis elvis = Elvis.INSTANCE;
         elvis.leaveTheBuilding();
