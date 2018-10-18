d7d6db641a1d3776ceecf1604eaacdc1b68af563
Wegra Lee <wegra.lee@gmail.com>
2018-10-18 21:40:00 +0900

    한글화

--- src/effectivejava/chapter2/item6/RomanNumerals.java
+++ src/effectivejava/chapter2/item6/RomanNumerals.java
@@ -1,15 +1,15 @@
 package effectivejava.chapter2.item6;
 import java.util.regex.Pattern;
 
-// Reusing expensive object for improved performance (Pages 22 and 23)
+// 값비싼 객체를 재사용해 성능을 개선한다. (32쪽)
 public class RomanNumerals {
-    // Performance can be greatly improved! (Page 22)
+    // 코드 6-1 성능을 훨씬 더 끌어올릴 수 있다!
     static boolean isRomanNumeralSlow(String s) {
         return s.matches("^(?=.)M*(C[MD]|D?C{0,3})"
                 + "(X[CL]|L?X{0,3})(I[XV]|V?I{0,3})$");
     }
 
-    // Reusing expensive object for improved performance (Page 23)
+    // 코드 6-2 값비싼 객체를 재사용해 성능을 개선한다.
     private static final Pattern ROMAN = Pattern.compile(
             "^(?=.)M*(C[MD]|D?C{0,3})"
                     + "(X[CL]|L?X{0,3})(I[XV]|V?I{0,3})$");
@@ -26,13 +26,14 @@ public class RomanNumerals {
         for (int i = 0; i < numSets; i++) {
             long start = System.nanoTime();
             for (int j = 0; j < numReps; j++) {
-                b ^= isRomanNumeralSlow("MCMLXXVI");  // Change Slow to Fast to see performance difference
+                // 성능 차이를 확인하려면 xxxSlow 메서드를 xxxFast 메서드로 바꿔 실행해보자.
+                b ^= isRomanNumeralSlow("MCMLXXVI");
             }
             long end = System.nanoTime();
             System.out.println(((end - start) / (1_000. * numReps)) + " μs.");
         }
 
-        // Prevents VM from optimizing away everything.
+        // VM이 최적화하지 못하게 막는 코드
         if (!b)
             System.out.println();
     }
