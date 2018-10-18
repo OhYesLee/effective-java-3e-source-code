2276583acb02fdcda0f491c6553cb8219c53056e
Wegra Lee <wegra.lee@gmail.com>
2018-10-18 21:44:55 +0900

    한글화

--- src/effectivejava/chapter2/item8/Room.java
+++ src/effectivejava/chapter2/item8/Room.java
@@ -2,11 +2,11 @@ package effectivejava.chapter2.item8;
 
 import java.lang.ref.Cleaner;
 
-// An autocloseable class using a cleaner as a safety net (Page 32)
+// 코드 8-1 cleaner를 안전망으로 활용하는 AutoCloseable 클래스 (44쪽)
 public class Room implements AutoCloseable {
     private static final Cleaner cleaner = Cleaner.create();
 
-    // Resource that requires cleaning. Must not refer to Room!
+    // 청소가 필요한 자원. 절대 Room을 참조해서는 안 된다!
     private static class State implements Runnable {
         int numJunkPiles; // Number of junk piles in this room
 
@@ -14,17 +14,17 @@ public class Room implements AutoCloseable {
             this.numJunkPiles = numJunkPiles;
         }
 
-        // Invoked by close method or cleaner
+        // close 메서드나 cleaner가 호출한다.
         @Override public void run() {
             System.out.println("Cleaning room");
             numJunkPiles = 0;
         }
     }
 
-    // The state of this room, shared with our cleanable
+    // 방의 상태. cleanable과 공유한다.
     private final State state;
 
-    // Our cleanable. Cleans the room when it’s eligible for gc
+    // cleanable 객체. 수거 대상이 되면 방을 청소한다.
     private final Cleaner.Cleanable cleanable;
 
     public Room(int numJunkPiles) {
