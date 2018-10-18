d76a96a3f957ae88e93831cd9b71e3f3206866ac
Wegra Lee <wegra.lee@gmail.com>
2018-10-18 21:15:04 +0900

    한글 번역

--- src/effectivejava/chapter2/item2/telescopingconstructor/NutritionFacts.java
+++ src/effectivejava/chapter2/item2/telescopingconstructor/NutritionFacts.java
@@ -1,13 +1,13 @@
 package effectivejava.chapter2.item2.telescopingconstructor;
 
-// Telescoping constructor pattern - does not scale well! (Pages 10-11)
+// 코드 2-1 점층적 생성자 패턴 - 확장하기 어렵다! (14~15쪽)
 public class NutritionFacts {
-    private final int servingSize;  // (mL)            required
-    private final int servings;     // (per container) required
-    private final int calories;     // (per serving)   optional
-    private final int fat;          // (g/serving)     optional
-    private final int sodium;       // (mg/serving)    optional
-    private final int carbohydrate; // (g/serving)     optional
+    private final int servingSize;  // (mL, 1회 제공량)     필수
+    private final int servings;     // (회, 총 n회 제공량)  필수
+    private final int calories;     // (1회 제공량당)       선택
+    private final int fat;          // (g/1회 제공량)       선택
+    private final int sodium;       // (mg/1회 제공량)      선택
+    private final int carbohydrate; // (g/1회 제공량)       선택
 
     public NutritionFacts(int servingSize, int servings) {
         this(servingSize, servings, 0);
