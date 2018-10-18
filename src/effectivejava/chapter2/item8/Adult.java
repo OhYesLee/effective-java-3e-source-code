c1f9884951317584c746ec449f4afb84e4564a8d
Wegra Lee <wegra.lee@gmail.com>
2018-10-18 21:47:47 +0900

    한글화

--- src/effectivejava/chapter2/item8/Adult.java
+++ src/effectivejava/chapter2/item8/Adult.java
@@ -1,10 +1,10 @@
 package effectivejava.chapter2.item8;
 
-// Well-behaved client of resource with cleaner safety-net (Page 33)
+// cleaner를 안전망으로 갖춘 잘 작동하는 클라이언트 (45쪽)
 public class Adult {
     public static void main(String[] args) {
         try (Room myRoom = new Room(7)) {
-            System.out.println("Goodbye");
+            System.out.println("안녕~");
         }
     }
 }
