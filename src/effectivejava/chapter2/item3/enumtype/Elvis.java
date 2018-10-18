8670b1b66911cdd4d8927046d70a6960954e64ac
Wegra Lee <wegra.lee@gmail.com>
2018-10-18 21:34:52 +0900

    한글화

--- src/effectivejava/chapter2/item3/enumtype/Elvis.java
+++ src/effectivejava/chapter2/item3/enumtype/Elvis.java
@@ -1,14 +1,14 @@
 package effectivejava.chapter2.item3.enumtype;
 
-// Enum singleton - the preferred approach (Page 18)
+// 열거 타입 방식의 싱글턴 - 바람직한 방법 (25쪽)
 public enum Elvis {
     INSTANCE;
 
     public void leaveTheBuilding() {
-        System.out.println("Whoa baby, I'm outta here!");
+        System.out.println("기다려 자기야, 지금 나갈께!");
     }
 
-    // This code would normally appear outside the class!
+    // 이 메서드는 보통 클래스 바깥(다른 클래스)에 작성해야 한다!
     public static void main(String[] args) {
         Elvis elvis = Elvis.INSTANCE;
         elvis.leaveTheBuilding();
