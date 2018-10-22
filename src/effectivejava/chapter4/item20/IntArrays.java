432afff3691897a6c35d6bde1ad54e34bbe90a3e
Wegra Lee <wegra.lee@gmail.com>
2018-10-22 21:38:02 +0900

    한글화

--- src/effectivejava/chapter4/item20/IntArrays.java
+++ src/effectivejava/chapter4/item20/IntArrays.java
@@ -1,22 +1,22 @@
 package effectivejava.chapter4.item20;
 import java.util.*;
 
-// Concrete implementation built atop skeletal implementation (Page 101)
+// 코드 20-1 골격 구현을 사용해 완성한 구체 클래스 (133쪽)
 public class IntArrays {
     static List<Integer> intArrayAsList(int[] a) {
         Objects.requireNonNull(a);
 
-        // The diamond operator is only legal here in Java 9 and later
-        // If you're using an earlier release, specify <Integer>
+        // 다이아몬드 연산자를 이렇게 사용하는 건 자바 9부터 가능하다.
+        // 더 낮은 버전을 사용한다면 <Integer>로 수정하자.
         return new AbstractList<>() {
             @Override public Integer get(int i) {
-                return a[i];  // Autoboxing (Item 6)
+                return a[i];  // 오토박싱(아이템 6)
             }
 
             @Override public Integer set(int i, Integer val) {
                 int oldVal = a[i];
-                a[i] = val;     // Auto-unboxing
-                return oldVal;  // Autoboxing
+                a[i] = val;     // 오토언박싱
+                return oldVal;  // 오토박싱
             }
 
             @Override public int size() {
