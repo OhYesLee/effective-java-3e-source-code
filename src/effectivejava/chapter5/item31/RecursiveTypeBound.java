ae7cc783cc10e6980c25e61915c76036b4edde24
Wegra Lee <wegra.lee@gmail.com>
2018-10-23 06:41:24 +0900

    한글화

--- src/effectivejava/chapter5/item31/RecursiveTypeBound.java
+++ src/effectivejava/chapter5/item31/RecursiveTypeBound.java
@@ -1,12 +1,12 @@
 package effectivejava.chapter5.item31;
 import java.util.*;
 
-// Using a recursive type bound with wildcards (Page 143)
+// 와일드카드 타입을 사용해 재귀적 타입 한정을 다듬었다. (187쪽)
 public class RecursiveTypeBound {
     public static <E extends Comparable<? super E>> E max(
         List<? extends E> list) {
         if (list.isEmpty())
-            throw new IllegalArgumentException("Empty list");
+            throw new IllegalArgumentException("빈 리스트");
 
         E result = null;
         for (E e : list)
