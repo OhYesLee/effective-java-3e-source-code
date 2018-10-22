d4df37cfead09ca4803545eb16ddda40c70a8a05
Wegra Lee <wegra.lee@gmail.com>
2018-10-23 06:55:47 +0900

    한글화

--- src/effectivejava/chapter5/item33/Favorites.java
+++ src/effectivejava/chapter5/item33/Favorites.java
@@ -1,8 +1,9 @@
 package effectivejava.chapter5.item33;
 import java.util.*;
 
-// Typesafe heterogeneous container pattern (Pages 151-4)
+// 타입 안전 이종 컨테이너 패턴 (199-202쪽)
 public class Favorites {
+    // 코드 33-3 타입 안전 이종 컨테이너 패턴 - 구현 (200쪽)
     private Map<Class<?>, Object> favorites = new HashMap<>();
 
     public <T> void putFavorite(Class<T> type, T instance) {
@@ -13,19 +14,23 @@ public class Favorites {
         return type.cast(favorites.get(type));
     }
 
-//    // Achieving runtime type safety with a dynamic cast
+//    // 코드 33-4 동적 형변환으로 런타임 타입 안전성 확보 (202쪽)
 //    public <T> void putFavorite(Class<T> type, T instance) {
 //        favorites.put(Objects.requireNonNull(type), type.cast(instance));
 //    }
 
+    // 코드 33-2 타입 안전 이종 컨테이너 패턴 - 클라이언트 (199쪽)
     public static void main(String[] args) {
         Favorites f = new Favorites();
+        
         f.putFavorite(String.class, "Java");
         f.putFavorite(Integer.class, 0xcafebabe);
         f.putFavorite(Class.class, Favorites.class);
+       
         String favoriteString = f.getFavorite(String.class);
         int favoriteInteger = f.getFavorite(Integer.class);
         Class<?> favoriteClass = f.getFavorite(Class.class);
+        
         System.out.printf("%s %x %s%n", favoriteString,
                 favoriteInteger, favoriteClass.getName());
     }
