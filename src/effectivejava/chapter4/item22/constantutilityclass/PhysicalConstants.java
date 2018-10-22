555bca2d3a8fe43f9270d99b279493902a6962a0
Wegra Lee <wegra.lee@gmail.com>
2018-10-22 21:40:08 +0900

    한글화

--- src/effectivejava/chapter4/item22/constantutilityclass/PhysicalConstants.java
+++ src/effectivejava/chapter4/item22/constantutilityclass/PhysicalConstants.java
@@ -1,15 +1,15 @@
 package effectivejava.chapter4.item22.constantutilityclass;
 
-// Constant utility class (Page 108)
+// 코드 22-2 상수 유틸리티 클래스 (140쪽)
 public class PhysicalConstants {
-  private PhysicalConstants() { }  // Prevents instantiation
+  private PhysicalConstants() { }  // 인스턴스화 방지
 
-  // Avogadro's number (1/mol)
+  // 아보가드로 수 (1/몰)
   public static final double AVOGADROS_NUMBER = 6.022_140_857e23;
 
-  // Boltzmann constant (J/K)
+  // 볼츠만 상수 (J/K)
   public static final double BOLTZMANN_CONST  = 1.380_648_52e-23;
 
-  // Mass of the electron (kg)
+  // 전자 질량 (kg)
   public static final double ELECTRON_MASS    = 9.109_383_56e-31;
 }
