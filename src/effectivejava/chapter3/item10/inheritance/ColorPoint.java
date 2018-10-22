38e0dd7c7fdb2dc659dc986b645d6b7401e0f956
Wegra Lee <wegra.lee@gmail.com>
2018-10-22 20:54:28 +0900

    한글화

--- src/effectivejava/chapter3/item10/inheritance/ColorPoint.java
+++ src/effectivejava/chapter3/item10/inheritance/ColorPoint.java
@@ -3,7 +3,7 @@ package effectivejava.chapter3.item10.inheritance;
 import effectivejava.chapter3.item10.Color;
 import effectivejava.chapter3.item10.Point;
 
-// Attempting to add a value component to Point (Page 41)
+// Point에 값 컴포넌트(color)를 추가 (56쪽)
 public class ColorPoint extends Point {
     private final Color color;
 
@@ -12,33 +12,33 @@ public class ColorPoint extends Point {
         this.color = color;
     }
 
-    // Broken - violates symmetry!  (Page 41)
+    // 코드 10-2 잘못된 코드 - 대칭성 위배! (57쪽)
     @Override public boolean equals(Object o) {
         if (!(o instanceof ColorPoint))
             return false;
         return super.equals(o) && ((ColorPoint) o).color == color;
     }
 
-//    // Broken - violates transitivity! (page 42)
+//    // 코드 10-3 잘못된 코드 - 추이성 위배! (57쪽)
 //    @Override public boolean equals(Object o) {
 //        if (!(o instanceof Point))
 //            return false;
 //
-//        // If o is a normal Point, do a color-blind comparison
+//        // o가 일반 Point면 색상을 무시하고 비교한다.
 //        if (!(o instanceof ColorPoint))
 //            return o.equals(this);
 //
-//        // o is a ColorPoint; do a full comparison
+//        // o가 ColorPoint면 색상까지 비교한다.
 //        return super.equals(o) && ((ColorPoint) o).color == color;
 //    }
 
     public static void main(String[] args) {
-        // First equals function violates symmetry (Page 42)
+        // 첫 번째 equals 메서드(코드 10-2)는 대칭성을 위배한다. (57쪽)
         Point p = new Point(1, 2);
         ColorPoint cp = new ColorPoint(1, 2, Color.RED);
         System.out.println(p.equals(cp) + " " + cp.equals(p));
 
-        // Second equals function violates transitivity (Page 42)
+        // 두 번째 equals 메서드(코드 10-3)는 추이성을 위배한다. (57쪽)
         ColorPoint p1 = new ColorPoint(1, 2, Color.RED);
         Point p2 = new Point(1, 2);
         ColorPoint p3 = new ColorPoint(1, 2, Color.BLUE);
