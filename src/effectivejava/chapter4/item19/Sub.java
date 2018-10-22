434fd7cccca68c780c59200ad6d3604566db0901
Wegra Lee <wegra.lee@gmail.com>
2018-10-22 21:34:45 +0900

    한글화

--- src/effectivejava/chapter4/item19/Sub.java
+++ src/effectivejava/chapter4/item19/Sub.java
@@ -2,16 +2,16 @@ package effectivejava.chapter4.item19;
 
 import java.time.Instant;
 
-// Demonstration of what can go wrong when you override a method  called from constructor (Page 96)
+// 생성자에서 호출하는 메서드를 재정의했을 때의 문제를 보여준다. (126쪽)
 public final class Sub extends Super {
-    // Blank final, set by constructor
+    // 초기화되지 않은 final 필드. 생성자에서 초기화한다.
     private final Instant instant;
 
     Sub() {
         instant = Instant.now();
     }
 
-    // Overriding method invoked by superclass constructor
+    // 재정의 가능 메서드. 상위 클래스의 생성자가 호출한다.
     @Override public void overrideMe() {
         System.out.println(instant);
     }
