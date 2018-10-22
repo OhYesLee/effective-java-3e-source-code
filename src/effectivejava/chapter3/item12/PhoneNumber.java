73a84d2d2f297c7febf911963a839cc7ab81a46d
Wegra Lee <wegra.lee@gmail.com>
2018-10-22 21:08:39 +0900

    한글화

--- src/effectivejava/chapter3/item12/PhoneNumber.java
+++ src/effectivejava/chapter3/item12/PhoneNumber.java
@@ -1,13 +1,13 @@
 package effectivejava.chapter3.item12;
 
-// Adding a toString method to PhoneNumber (page 52)
+// PhoneNumber에 toString 메서드 추가 (75쪽)
 public final class PhoneNumber {
     private final short areaCode, prefix, lineNum;
 
     public PhoneNumber(int areaCode, int prefix, int lineNum) {
-        this.areaCode = rangeCheck(areaCode, 999, "area code");
-        this.prefix   = rangeCheck(prefix,   999, "prefix");
-        this.lineNum  = rangeCheck(lineNum, 9999, "line num");
+        this.areaCode = rangeCheck(areaCode, 999, "지역코드");
+        this.prefix   = rangeCheck(prefix,   999, "프리픽스");
+        this.lineNum  = rangeCheck(lineNum, 9999, "가입자 번호");
     }
 
     private static short rangeCheck(int val, int max, String arg) {
@@ -34,16 +34,14 @@ public final class PhoneNumber {
     }
 
     /**
-     * Returns the string representation of this phone number.
-     * The string consists of twelve characters whose format is
-     * "XXX-YYY-ZZZZ", where XXX is the area code, YYY is the
-     * prefix, and ZZZZ is the line number. Each of the capital
-     * letters represents a single decimal digit.
+     * 이 전화번호의 문자열 표현을 반환한다.
+     * 이 문자열은 "XXX-YYY-ZZZZ" 형태의 12글자로 구성된다.
+     * XXX는 지역 코드, YYY는 프리픽스, ZZZZ는 가입자 번호다.
+     * 각각의 대문자는 10진수 숫자 하나를 나타낸다.
      *
-     * If any of the three parts of this phone number is too small
-     * to fill up its field, the field is padded with leading zeros.
-     * For example, if the value of the line number is 123, the last
-     * four characters of the string representation will be "0123".
+     * 전화번호의 각 부분의 값이 너무 작아서 자릿수를 채울 수 없다면,
+     * 앞에서부터 0으로 채워나간다. 예컨대 가입자 번호가 123이라면
+     * 전화번호의 마지막 네 문자는 "0123"이 된다.
      */
 //    @Override public String toString() {
 //        return String.format("%03d-%03d-%04d",
@@ -52,6 +50,6 @@ public final class PhoneNumber {
 
     public static void main(String[] args) {
         PhoneNumber jenny = new PhoneNumber(707, 867, 5309);
-        System.out.println("Jenny's number: " + jenny);
+        System.out.println("제니의 번호: " + jenny);
     }
 }
