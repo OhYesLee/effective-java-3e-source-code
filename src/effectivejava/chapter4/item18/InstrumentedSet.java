557b0a9d1d5493747789b2b7a86b12b414f5546d
Wegra Lee <wegra.lee@gmail.com>
2018-10-22 21:31:20 +0900

    한글화

--- src/effectivejava/chapter4/item18/InstrumentedSet.java
+++ src/effectivejava/chapter4/item18/InstrumentedSet.java
@@ -1,7 +1,7 @@
 package effectivejava.chapter4.item18;
 import java.util.*;
 
-// Wrapper class - uses composition in place of inheritance  (Page 90)
+// 코드 18-2 래퍼 클래스 - 상속 대신 컴포지션을 사용했다. (117-118쪽)
 public class InstrumentedSet<E> extends ForwardingSet<E> {
     private int addCount = 0;
 
@@ -23,7 +23,7 @@ public class InstrumentedSet<E> extends ForwardingSet<E> {
 
     public static void main(String[] args) {
         InstrumentedSet<String> s = new InstrumentedSet<>(new HashSet<>());
-        s.addAll(List.of("Snap", "Crackle", "Pop"));
+        s.addAll(List.of("틱", "탁탁", "펑"));
         System.out.println(s.getAddCount());
     }
 }
