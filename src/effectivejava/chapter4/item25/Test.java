c6e5e3f6262aaa0f0c19b61d09b801a316baa51c
Wegra Lee <wegra.lee@gmail.com>
2018-10-22 21:46:57 +0900

    한글화

--- src/effectivejava/chapter4/item25/Test.java
+++ src/effectivejava/chapter4/item25/Test.java
@@ -1,6 +1,6 @@
 package effectivejava.chapter4.item25;
 
-// Static member classes instead of multiple top-level classes (Page 116)
+// 코드 25-3 톱레벨 클래스들을 정적 멤버 클래스로 바꿔본 모습 (151-152쪽)
 public class Test {
     public static void main(String[] args) {
         System.out.println(Utensil.NAME + Dessert.NAME);
