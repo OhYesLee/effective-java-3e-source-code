bc1d7ffb6c830615698b7d70b7bbd7e143fe235e
Wegra Lee <wegra.lee@gmail.com>
2018-10-23 06:14:36 +0900

    한글화

--- src/effectivejava/chapter5/item29/technqiue2/Stack.java
+++ src/effectivejava/chapter5/item29/technqiue2/Stack.java
@@ -3,7 +3,7 @@ package effectivejava.chapter5.item29.technqiue2;
 import java.util.Arrays;
 import effectivejava.chapter5.item29.EmptyStackException;
 
-// Generic stack using Object[] (Pages 130-3)
+// Object[]를 이용한 제네릭 Stack (170-174쪽)
 public class Stack<E> {
     private Object[] elements;
     private int size = 0;
@@ -18,16 +18,17 @@ public class Stack<E> {
         elements[size++] = e;
     }
 
-    // Appropriate suppression of unchecked warning
+    // 코드 29-4 배열을 사용한 코드를 제네릭으로 만드는 방법 2 (173쪽)
+    // 비검사 경고를 적절히 숨긴다.
     public E pop() {
         if (size == 0)
             throw new EmptyStackException();
 
-        // push requires elements to be of type E, so cast is correct
+        // push에서 E 타입만 허용하므로 이 형변환은 안전하다.
         @SuppressWarnings("unchecked") E result =
                 (E) elements[--size];
 
-        elements[size] = null; // Eliminate obsolete reference
+        elements[size] = null; // 다 쓴 참조 해제
         return result;
     }
 
@@ -40,7 +41,7 @@ public class Stack<E> {
             elements = Arrays.copyOf(elements, 2 * size + 1);
     }
 
-    // Little program to exercise our generic Stack
+    // 코드 29-5 제네릭 Stack을 사용하는 맛보기 프로그램 (174쪽)
     public static void main(String[] args) {
         Stack<String> stack = new Stack<>();
         for (String arg : args)
