cb5dffe3757dc1575cf0ebb4e25651b763a1a814
Wegra Lee <wegra.lee@gmail.com>
2018-10-22 21:11:07 +0900

    한글화

--- src/effectivejava/chapter3/item13/PhoneNumber.java
+++ src/effectivejava/chapter3/item13/PhoneNumber.java
@@ -3,14 +3,14 @@ package effectivejava.chapter3.item13;
 import java.util.HashMap;
 import java.util.Map;
 
-// Adding a clone method to PhoneNumber (page 59)
+// PhoneNumber에 clone 메서드 추가 (79쪽)
 public final class PhoneNumber implements Cloneable {
     private final short areaCode, prefix, lineNum;
 
     public PhoneNumber(int areaCode, int prefix, int lineNum) {
-        this.areaCode = rangeCheck(areaCode, 999, "area code");
-        this.prefix   = rangeCheck(prefix,   999, "prefix");
-        this.lineNum  = rangeCheck(lineNum, 9999, "line num");
+        this.areaCode = rangeCheck(areaCode, 999, "지역코드");
+        this.prefix   = rangeCheck(prefix,   999, "프리픽스");
+        this.lineNum  = rangeCheck(lineNum, 9999, "가입자 번호");
     }
 
     private static short rangeCheck(int val, int max, String arg) {
@@ -37,35 +37,33 @@ public final class PhoneNumber implements Cloneable {
     }
 
     /**
-     * Returns the string representation of this phone number.
-     * The string consists of twelve characters whose format is
-     * "XXX-YYY-ZZZZ", where XXX is the area code, YYY is the
-     * prefix, and ZZZZ is the line number. Each of the capital
-     * letters represents a single decimal digit.
+     * 이 전화번호의 문자열 표현을 반환한다.
+     * 이 문자열은 "XXX-YYY-ZZZZ" 형태의 12글자로 구성된다.
+     * XXX는 지역 코드, YYY는 프리픽스, ZZZZ는 가입자 번호다.
+     * 각각의 대문자는 10진수 숫자 하나를 나타낸다.
      *
-     * If any of the three parts of this phone number is too small
-     * to fill up its field, the field is padded with leading zeros.
-     * For example, if the value of the line number is 123, the last
-     * four characters of the string representation will be "0123".
+     * 전화번호의 각 부분의 값이 너무 작아서 자릿수를 채울 수 없다면,
+     * 앞에서부터 0으로 채워나간다. 예컨대 가입자 번호가 123이라면
+     * 전화번호의 마지막 네 문자는 "0123"이 된다.
      */
     @Override public String toString() {
         return String.format("%03d-%03d-%04d",
                 areaCode, prefix, lineNum);
     }
 
-    // Clone method for class with no references to mutable state (Page 59)
+    // 코드 13-1 가변 상태를 참조하지 않는 클래스용 clone 메서드 (79쪽)
     @Override public PhoneNumber clone() {
         try {
             return (PhoneNumber) super.clone();
         } catch (CloneNotSupportedException e) {
-            throw new AssertionError();  // Can't happen
+            throw new AssertionError();  // 일어날 수 없는 일이다.
         }
     }
 
     public static void main(String[] args) {
         PhoneNumber pn = new PhoneNumber(707, 867, 5309);
         Map<PhoneNumber, String> m = new HashMap<>();
-        m.put(pn, "Jenny");
+        m.put(pn, "제니");
         System.out.println(m.get(pn.clone()));
     }
 }
