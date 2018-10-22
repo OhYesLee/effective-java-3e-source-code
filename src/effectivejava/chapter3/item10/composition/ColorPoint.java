5ebd99936e5fd369a025379289342b13fc64ec49
Wegra Lee <wegra.lee@gmail.com>
2018-10-22 21:01:04 +0900

    한글화

--- src/effectivejava/chapter3/item10/composition/ColorPoint.java
+++ src/effectivejava/chapter3/item10/composition/ColorPoint.java
@@ -5,7 +5,7 @@ import effectivejava.chapter3.item10.Point;
 
 import java.util.Objects;
 
-// Adds a value component without violating the equals contract (page 44)
+// 코드 10-5 equals 규약을 지키면서 값 추가하기 (60쪽)
 public class ColorPoint {
     private final Point point;
     private final Color color;
@@ -16,7 +16,7 @@ public class ColorPoint {
     }
 
     /**
-     * Returns the point-view of this color point.
+     * 이 ColorPoint의 Point 뷰를 반환한다.
      */
     public Point asPoint() {
         return point;
