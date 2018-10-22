117c2c7dd5f9bf0b8782c81bd114f4e01bc674d7
Wegra Lee <wegra.lee@gmail.com>
2018-10-22 21:45:54 +0900

    한글화

--- src/effectivejava/chapter4/item25/Dessert.java
+++ src/effectivejava/chapter4/item25/Dessert.java
@@ -1,6 +1,6 @@
 package effectivejava.chapter4.item25;
 
-// Two classes defined in one file. Don't ever do this! (Page 115)
+// 코드 25-2 두 클래스가 한 파일(Dessert.java)에 정의되었다. 따라 하지 말 것! (151쪽)
 //class Utensil {
 //    static final String NAME = "pot";
 //}
