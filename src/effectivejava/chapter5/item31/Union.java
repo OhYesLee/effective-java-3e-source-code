9625f61594048d93c01af723d82abc6e384caa7e
Wegra Lee <wegra.lee@gmail.com>
2018-10-23 06:36:57 +0900

    한글화

--- src/effectivejava/chapter5/item31/Union.java
+++ src/effectivejava/chapter5/item31/Union.java
@@ -1,7 +1,7 @@
 package effectivejava.chapter5.item31;
 import java.util.*;
 
-// Generic union method with wildcard types for enhanced flexibility (Pages 142-3)
+// 코드 30-2의 제네릭 union 메서드에 와일드카드 타입을 적용해 유연성을 높였다. (185-186쪽)
 public class Union {
     public static <E> Set<E> union(Set<? extends E> s1,
                                    Set<? extends E> s2) {
@@ -10,7 +10,7 @@ public class Union {
         return result;
     }
 
-    // Simple program to exercise flexible generic staticfactory
+    // 향상된 유연성을 확인해주는 맛보기 프로그램 (185쪽)
     public static void main(String[] args) {
         Set<Integer> integers = new HashSet<>();
         integers.add(1); 
@@ -24,7 +24,7 @@ public class Union {
 
         Set<Number> numbers = union(integers, doubles);
 
-//      // Explicit type parameter - required prior to Java 8
+//      // 코드 31-6 자바 7까지는 명시적 타입 인수를 사용해야 한다. (186쪽)
 //      Set<Number> numbers = Union.<Number>union(integers, doubles);
 
         System.out.println(numbers);
