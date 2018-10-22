e5710678b1ed86e6741b41153103965afd090348
Wegra Lee <wegra.lee@gmail.com>
2018-10-22 21:43:12 +0900

    한글화

--- src/effectivejava/chapter4/item23/hierarchy/Square.java
+++ src/effectivejava/chapter4/item23/hierarchy/Square.java
@@ -1,6 +1,6 @@
 package effectivejava.chapter4.item23.hierarchy;
 
-// Class hierarchy replacement for a tagged class  (Page 110-11)
+// 태그 달린 클래스를 클래스 계층구조로 변환 (145쪽)
 class Square extends Rectangle {
     Square(double side) {
         super(side, side);
