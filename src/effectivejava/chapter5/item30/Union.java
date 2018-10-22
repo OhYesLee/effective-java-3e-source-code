1bff0a5c2cb6d915d2f22c881de45df4708e877c
Wegra Lee <wegra.lee@gmail.com>
2018-10-23 06:18:12 +0900

    한글화

--- src/effectivejava/chapter5/item30/Union.java
+++ src/effectivejava/chapter5/item30/Union.java
@@ -1,20 +1,20 @@
 package effectivejava.chapter5.item30;
 import java.util.*;
 
-// Generic union method and program to exercise it  (Pages 135-6)
+// 제네릭 union 메서드와 테스트 프로그램 (177쪽)
 public class Union {
 
-    // Generic method
+    // 코드 30-2 제네릭 메서드 (177쪽)
     public static <E> Set<E> union(Set<E> s1, Set<E> s2) {
         Set<E> result = new HashSet<>(s1);
         result.addAll(s2);
         return result;
     }
 
-    // Simple program to exercise generic method
+    // 코드 30-3 제네릭 메서드를 활용하는 간단한 프로그램 (177쪽)
     public static void main(String[] args) {
-        Set<String> guys = Set.of("Tom", "Dick", "Harry");
-        Set<String> stooges = Set.of("Larry", "Moe", "Curly");
+        Set<String> guys = Set.of("톰", "딕", "해리");
+        Set<String> stooges = Set.of("래리", "모에", "컬리");
         Set<String> aflCio = union(guys, stooges);
         System.out.println(aflCio);
     }
