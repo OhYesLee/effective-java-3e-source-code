2ee8eb2fa318526996c72b1f47d12e2e2026995b
Wegra Lee <wegra.lee@gmail.com>
2018-10-23 06:44:36 +0900

    한글화

--- src/effectivejava/chapter5/item32/Dangerous.java
+++ src/effectivejava/chapter5/item32/Dangerous.java
@@ -2,13 +2,13 @@ package effectivejava.chapter5.item32;
 
 import java.util.List;
 
-// It is unsafe to store a value in a generic varargs array parameter (Page 146)
+// 제네릭 varargs 배열 매개변수에 값을 저장하는 것은 안전하지 않다. (191-192쪽)
 public class Dangerous {
-    // Mixing generics and varargs can violate type safety!
+    // 코드 32-1 제네릭과 varargs를 혼용하면 타입 안전성이 깨진다! (191-192쪽)
     static void dangerous(List<String>... stringLists) {
         List<Integer> intList = List.of(42);
         Object[] objects = stringLists;
-        objects[0] = intList; // Heap pollution
+        objects[0] = intList; // 힙 오염 발생
         String s = stringLists[0].get(0); // ClassCastException
     }
 
