94db8433dbaba3e22dd290a6ac442a34ce22c986
Wegra Lee <wegra.lee@gmail.com>
2018-10-22 20:44:15 +0900

    한글화

--- src/effectivejava/chapter3/item10/CaseInsensitiveString.java
+++ src/effectivejava/chapter3/item10/CaseInsensitiveString.java
@@ -4,7 +4,7 @@ import java.util.ArrayList;
 import java.util.List;
 import java.util.Objects;
 
-// Broken - violates symmetry!  (Page 39)
+// 코드 10-1 잘못된 코드 - 대칭성 위배! (54-55쪽)
 public final class CaseInsensitiveString {
     private final String s;
 
@@ -12,17 +12,17 @@ public final class CaseInsensitiveString {
         this.s = Objects.requireNonNull(s);
     }
 
-    // Broken - violates symmetry!
+    // 대칭성 위배!
     @Override public boolean equals(Object o) {
         if (o instanceof CaseInsensitiveString)
             return s.equalsIgnoreCase(
                     ((CaseInsensitiveString) o).s);
-        if (o instanceof String)  // One-way interoperability!
+        if (o instanceof String)  // 한 방향으로만 작동한다!
             return s.equalsIgnoreCase((String) o);
         return false;
     }
 
-    // Demonstration of the problem (Page 40)
+    // 문제 시연 (55쪽)
     public static void main(String[] args) {
         CaseInsensitiveString cis = new CaseInsensitiveString("Polish");
         String s = "polish";
@@ -33,7 +33,7 @@ public final class CaseInsensitiveString {
         System.out.println(list.contains(s));
     }
 
-//    // Fixed equals method (Page 40)
+//    // 수정한 equals 메서드 (56쪽)
 //    @Override public boolean equals(Object o) {
 //        return o instanceof CaseInsensitiveString &&
 //                ((CaseInsensitiveString) o).s.equalsIgnoreCase(s);
