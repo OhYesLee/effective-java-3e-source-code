6448bcb0ebe7d85dc73f573c8208d1c2eb573e8e
Wegra Lee <wegra.lee@gmail.com>
2018-10-22 21:33:36 +0900

    한글화

--- src/effectivejava/chapter4/item19/Super.java
+++ src/effectivejava/chapter4/item19/Super.java
@@ -1,8 +1,8 @@
 package effectivejava.chapter4.item19;
 
-// Class whose constructor invokes an overridable method. NEVER DO THIS! (Page 95)
+// 재정의 가능 메서드를 호출하는 생성자 - 따라 하지 말 것! (115쪽)
 public class Super {
-    // Broken - constructor invokes an overridable method
+    // 잘못된 예 - 생성자가 재정의 가능 메서드를 호출한다.
     public Super() {
         overrideMe();
     }
