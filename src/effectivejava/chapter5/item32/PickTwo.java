73a85cf413382a4c7c68e28e384c34d75c458c53
Wegra Lee <wegra.lee@gmail.com>
2018-10-23 06:50:30 +0900

    한글화

--- src/effectivejava/chapter5/item32/PickTwo.java
+++ src/effectivejava/chapter5/item32/PickTwo.java
@@ -3,9 +3,9 @@ package effectivejava.chapter5.item32;
 import java.util.Arrays;
 import java.util.concurrent.ThreadLocalRandom;
 
-// Subtle heap pollution (Pages 147-8)
+// 미묘하 힙 오염 발생 (193-194쪽)
 public class PickTwo {
-    // UNSAFE - Exposes a reference to its generic parameter array!
+    // 코드 32-2 자신의 제네릭 매개변수 배열의 참조를 노출한다. - 안전하지 않다! (193쪽)
     static <T> T[] toArray(T... args) {
         return args;
     }
@@ -16,11 +16,11 @@ public class PickTwo {
             case 1: return toArray(a, c);
             case 2: return toArray(b, c);
         }
-        throw new AssertionError(); // Can't get here
+        throw new AssertionError(); // 도달할 수 없다.
     }
 
-    public static void main(String[] args) {
-        String[] attributes = pickTwo("Good", "Fast", "Cheap");
+    public static void main(String[] args) { // (194쪽)
+        String[] attributes = pickTwo("좋은", "빠른", "저렴한");
         System.out.println(Arrays.toString(attributes));
     }
 }
