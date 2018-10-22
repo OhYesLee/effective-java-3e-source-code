9be25996c43d8b8a5a12ef3a0977491d6e0316fc
Wegra Lee <wegra.lee@gmail.com>
2018-10-22 21:42:13 +0900

    한글화

--- src/effectivejava/chapter4/item23/hierarchy/Figure.java
+++ src/effectivejava/chapter4/item23/hierarchy/Figure.java
@@ -1,6 +1,6 @@
 package effectivejava.chapter4.item23.hierarchy;
 
-// Class hierarchy replacement for a tagged class  (Page 110-11)
+// 코드 23-2 태그 달린 클래스를 클래스 계층구조로 변환 (144쪽)
 abstract class Figure {
     abstract double area();
 }
