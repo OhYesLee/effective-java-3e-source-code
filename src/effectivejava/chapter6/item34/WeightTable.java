d1dec686751d80175be59f5df9eb1e09e75e46e9
Wegra Lee <wegra.lee@gmail.com>
2018-10-23 23:31:18 +0900

    한글화

--- src/effectivejava/chapter6/item34/WeightTable.java
+++ src/effectivejava/chapter6/item34/WeightTable.java
@@ -1,12 +1,12 @@
 package effectivejava.chapter6.item34;
 
-// Takes earth-weight and prints table of weights on all planets (Page 160)
+// 어떤 객체의 지구에서의 무게를 입력받아 여덟 행성에서의 무게를 출력한다. (212쪽)
 public class WeightTable {
    public static void main(String[] args) {
       double earthWeight = Double.parseDouble(args[0]);
       double mass = earthWeight / Planet.EARTH.surfaceGravity();
       for (Planet p : Planet.values())
-         System.out.printf("Weight on %s is %f%n",
-                 p, p.surfaceWeight(mass));
+         System.out.printf("%s에서의 무게는 %f이다.%n",
+                           p, p.surfaceWeight(mass));
    }
 }
