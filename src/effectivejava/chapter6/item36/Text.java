79b8e3cd42674a0a704600316b10412bdfd92c4d
Wegra Lee <wegra.lee@gmail.com>
2018-10-23 23:54:45 +0900

    한글화

--- src/effectivejava/chapter6/item36/Text.java
+++ src/effectivejava/chapter6/item36/Text.java
@@ -2,17 +2,17 @@ package effectivejava.chapter6.item36;
 
 import java.util.*;
 
-// EnumSet - a modern replacement for bit fields (Page 170)
+// 코드 36-2 EnumSet - 비트 필드를 대체하는 현대적 기법 (224쪽)
 public class Text {
     public enum Style {BOLD, ITALIC, UNDERLINE, STRIKETHROUGH}
 
-    // Any Set could be passed in, but EnumSet is clearly best
+    // 어떤 Set을 넘겨도 되나, EnumSet이 가장 좋다.
     public void applyStyles(Set<Style> styles) {
         System.out.printf("Applying styles %s to text%n",
                 Objects.requireNonNull(styles));
     }
 
-    // Sample use
+    // 사용 예
     public static void main(String[] args) {
         Text text = new Text();
         text.applyStyles(EnumSet.of(Style.BOLD, Style.ITALIC));
