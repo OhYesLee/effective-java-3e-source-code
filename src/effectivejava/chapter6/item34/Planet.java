7c71dd24e5157fd794fb65a35310fe0c2bf66055
Wegra Lee <wegra.lee@gmail.com>
2018-10-23 23:29:43 +0900

    한글화

--- src/effectivejava/chapter6/item34/Planet.java
+++ src/effectivejava/chapter6/item34/Planet.java
@@ -1,6 +1,6 @@
 package effectivejava.chapter6.item34;
 
-// Enum type with data and behavior  (159-160)
+// 코드 34-3 데이터와 메서드를 갖는 열거 타입 (211쪽)
 public enum Planet {
     MERCURY(3.302e+23, 2.439e6),
     VENUS  (4.869e+24, 6.052e6),
@@ -11,14 +11,14 @@ public enum Planet {
     URANUS (8.683e+25, 2.556e7),
     NEPTUNE(1.024e+26, 2.477e7);
 
-    private final double mass;           // In kilograms
-    private final double radius;         // In meters
-    private final double surfaceGravity; // In m / s^2
+    private final double mass;           // 질량(단위: 킬로그램)
+    private final double radius;         // 반지름(단위: 미터)
+    private final double surfaceGravity; // 표면중력(단위: m / s^2)
 
-    // Universal gravitational constant in m^3 / kg s^2
+    // 중력상수(단위: m^3 / kg s^2)
     private static final double G = 6.67300E-11;
 
-    // Constructor
+    // 생성자
     Planet(double mass, double radius) {
         this.mass = mass;
         this.radius = radius;
