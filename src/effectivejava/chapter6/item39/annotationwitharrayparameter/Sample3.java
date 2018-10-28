959d50bc58a140d82c38175d8c3a1f7ed906578c
Wegra Lee <wegra.lee@gmail.com>
2018-10-28 10:07:01 +0900

    한글화

--- src/effectivejava/chapter6/item39/annotationwitharrayparameter/Sample3.java
+++ src/effectivejava/chapter6/item39/annotationwitharrayparameter/Sample3.java
@@ -2,30 +2,30 @@ package effectivejava.chapter6.item39.annotationwitharrayparameter;
 
 import java.util.*;
 
-// Program containing an annotation with an array parameter (Page 185)
+// 배열 매개변수를 받는 애너테이션을 사용하는 프로그램 (242-243쪽)
 public class Sample3 {
-    // This variant can process annotations whose parameter is a single element (identical to those on page 183)
+    // 이 변형은 원소 하나짜리 매개변수를 받는 애너테이션도 처리할 수 있다. (241쪽 Sample2와 같음)
     @ExceptionTest(ArithmeticException.class)
-    public static void m1() {  // Test should pass
+    public static void m1() {  // 성공해야 한다.
         int i = 0;
         i = i / i;
     }
     @ExceptionTest(ArithmeticException.class)
-    public static void m2() {  // Should fail (wrong exception)
+    public static void m2() {  // 실패해야 한다. (다른 예외 발생)
         int[] a = new int[0];
         int i = a[1];
     }
     @ExceptionTest(ArithmeticException.class)
-    public static void m3() { }  // Should fail (no exception)
+    public static void m3() { }  // 실패해야 한다. (예외가 발생하지 않음)
 
-    // Code containing an annotation with an array parameter (Page 185)
+    // 코드 39-7 배열 매개변수를 받는 애너테이션을 사용하는 코드 (242-243쪽)
     @ExceptionTest({ IndexOutOfBoundsException.class,
                      NullPointerException.class })
-    public static void doublyBad() {   // Should pass
+    public static void doublyBad() {   // 성공해야 한다.
         List<String> list = new ArrayList<>();
 
-        // The spec permits this method to throw either
-        // IndexOutOfBoundsException or NullPointerException
+        // 자바 API 명세에 따르면 다음 메서드는 IndexOutOfBoundsException이나
+        // NullPointerException을 던질 수 있다.
         list.addAll(5, null);
     }
 }
