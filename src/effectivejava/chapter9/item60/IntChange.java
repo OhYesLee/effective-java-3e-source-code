d0fa43a7969efe5bd9e5c14acd4265a347a3b00a
Wegra Lee <wegra.lee@gmail.com>
2018-10-28 15:44:55 +0900

    한글화

--- src/effectivejava/chapter9/item60/IntChange.java
+++ src/effectivejava/chapter9/item60/IntChange.java
@@ -1,6 +1,7 @@
 package effectivejava.chapter9.item60;
 
 public class IntChange {
+    // 코드 60-3 정수 타입을 사용한 해법 (357쪽)
     public static void main(String[] args) {
         int itemsBought = 0;
         int funds = 100;
@@ -8,7 +9,7 @@ public class IntChange {
             funds -= price;
             itemsBought++;
         }
-        System.out.println(itemsBought + " items bought.");
-        System.out.println("Cash left over: " + funds + " cents");
+        System.out.println(itemsBought + "개 구입");
+        System.out.println("잔돈(센트): " + funds);
     }
 }
