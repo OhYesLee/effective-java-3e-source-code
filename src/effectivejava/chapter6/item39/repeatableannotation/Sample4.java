802e0bf7d092249850e4d5785e23dcf8f539a3ee
Wegra Lee <wegra.lee@gmail.com>
2018-10-28 10:13:18 +0900

    한글화

--- src/effectivejava/chapter6/item39/repeatableannotation/Sample4.java
+++ src/effectivejava/chapter6/item39/repeatableannotation/Sample4.java
@@ -3,31 +3,31 @@ package effectivejava.chapter6.item39.repeatableannotation;
 import java.util.ArrayList;
 import java.util.List;
 
-// Program containing repeatable annotations (Page 186)
+// 반복 가능한 애너테이션을 사용한 프로그램 (244쪽)
 public class Sample4 {
     @ExceptionTest(ArithmeticException.class)
-    public static void m1() {  // Test should pass
+    public static void m1() {  // 성공해야 한다.
         int i = 0;
         i = i / i;
     }
 
     @ExceptionTest(ArithmeticException.class)
-    public static void m2() {  // Should fail (wrong exception)
+    public static void m2() {  // 실패해야 한다. (다른 예외 발생)
         int[] a = new int[0];
         int i = a[1];
     }
 
     @ExceptionTest(ArithmeticException.class)
-    public static void m3() { }  // Should fail (no exception)
+    public static void m3() { }  // 실패해야 한다. (예외가 발생하지 않음)
 
-    // Code containing a repeated annotation (Page 186)
+    // 코드 39-9 반복 가능 애너테이션을 두 번 단 코드 (244쪽)
     @ExceptionTest(IndexOutOfBoundsException.class)
     @ExceptionTest(NullPointerException.class)
     public static void doublyBad() {
         List<String> list = new ArrayList<>();
 
-        // The spec permits this staticfactory to throw either
-        // IndexOutOfBoundsException or NullPointerException
+        // 자바 API 명세에 따르면 다음 메서드는 IndexOutOfBoundsException이나
+        // NullPointerException을 던질 수 있다.
         list.addAll(5, null);
     }
 }
