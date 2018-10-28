a6e7883368d65e092eab1478d74564deb0c84fd8
Wegra Lee <wegra.lee@gmail.com>
2018-10-28 15:45:57 +0900

    한글화

--- src/effectivejava/chapter9/item61/BrokenComparator.java
+++ src/effectivejava/chapter9/item61/BrokenComparator.java
@@ -1,16 +1,16 @@
 package effectivejava.chapter9.item61;
 import java.util.*;
 
-// Broken comparator - can you spot the flaw? - Page 273
+// 코드 61-1 잘못 구현된 비교자 - 문제를 찾아보자! (359쪽)
 public class BrokenComparator {
     public static void main(String[] args) {
 
 //        Comparator<Integer> naturalOrder =
 //                (i, j) -> (i < j) ? -1 : (i == j ? 0 : 1);
 
-       // Fixed Comparator - Page 274
+       // 코드 61-2 문제를 수정한 비교자 (359쪽)
         Comparator<Integer> naturalOrder = (iBoxed, jBoxed) -> {
-            int i = iBoxed, j = jBoxed; // Auto-unboxing
+            int i = iBoxed, j = jBoxed; // 오토박싱
             return i < j ? -1 : (i == j ? 0 : 1);
         };
 
