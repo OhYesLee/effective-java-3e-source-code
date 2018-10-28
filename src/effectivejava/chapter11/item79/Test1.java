fb32e3fff7c10a3406101f20040048eb6da49806
Wegra Lee <wegra.lee@gmail.com>
2018-10-28 16:07:20 +0900

    한글화

--- src/effectivejava/chapter11/item79/Test1.java
+++ src/effectivejava/chapter11/item79/Test1.java
@@ -1,7 +1,7 @@
 package effectivejava.chapter11.item79;
 import java.util.*;
 
-// Simple test of ObservableSet - Page 318
+// ObservableSet 동작 확인 #1 - 0부터 99까지 출력한다. (422쪽)
 public class Test1 {
     public static void main(String[] args) {
         ObservableSet<Integer> set =
