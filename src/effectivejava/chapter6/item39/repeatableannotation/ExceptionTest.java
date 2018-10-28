968c17792ee6d4652c369b3c65ba89758e49a05f
Wegra Lee <wegra.lee@gmail.com>
2018-10-28 10:10:04 +0900

    한글화

--- src/effectivejava/chapter6/item39/repeatableannotation/ExceptionTest.java
+++ src/effectivejava/chapter6/item39/repeatableannotation/ExceptionTest.java
@@ -2,7 +2,7 @@ package effectivejava.chapter6.item39.repeatableannotation;
 
 import java.lang.annotation.*;
 
-// Repeatable annotation type (Page 186)
+// 코드 39-8 반복 가능한 애너테이션 타입 (243-244쪽)
 @Retention(RetentionPolicy.RUNTIME)
 @Target(ElementType.METHOD)
 @Repeatable(ExceptionTestContainer.class)
