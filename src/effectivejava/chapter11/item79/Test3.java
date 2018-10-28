3133162329fb8471ba136b3916b31c74ab7403dd
Wegra Lee <wegra.lee@gmail.com>
2018-10-28 16:11:20 +0900

    한글화

--- src/effectivejava/chapter11/item79/Test3.java
+++ src/effectivejava/chapter11/item79/Test3.java
@@ -4,13 +4,13 @@ import java.util.concurrent.ExecutionException;
 import java.util.concurrent.ExecutorService;
 import java.util.concurrent.Executors;
 
-// Simple test of ObservableSet - Page 319
+// ObservableSet 동작 확인 #3
 public class Test3 {
     public static void main(String[] args) {
         ObservableSet<Integer> set =
                 new ObservableSet<>(new HashSet<>());
 
-// Observer that uses a background thread needlessly
+// 코드 79-2 쓸데없이 백그라운드 스레드를 사용하는 관찰자 (423쪽)
         set.addObserver(new SetObserver<>() {
             public void added(ObservableSet<Integer> s, Integer e) {
                 System.out.println(e);
