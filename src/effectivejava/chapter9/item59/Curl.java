109f8b459366dc65b36764ed969a482dd0d421d5
Wegra Lee <wegra.lee@gmail.com>
2018-10-28 15:42:06 +0900

    한글화

--- src/effectivejava/chapter9/item59/Curl.java
+++ src/effectivejava/chapter9/item59/Curl.java
@@ -4,7 +4,7 @@ import java.io.IOException;
 import java.io.InputStream;
 import java.net.URL;
 
-// Printing the contents of a URL with transferTo, added in Java 9 (Page 269)
+// 코드 59-2 transferTo 메서드를 이용해 URL의 내용 가져오기 - 자바 9부터 가능하다. (353쪽)
 public class Curl {
     public static void main(String[] args) throws IOException {
         try (InputStream in = new URL(args[0]).openStream()) {
