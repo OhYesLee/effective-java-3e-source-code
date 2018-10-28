44380a9f2b4fb15d2cf7c53d76eeb327a8b25ef8
Wegra Lee <wegra.lee@gmail.com>
2018-10-28 11:24:35 +0900

    한글화

--- src/effectivejava/chapter7/item47/Adapters.java
+++ src/effectivejava/chapter7/item47/Adapters.java
@@ -3,14 +3,14 @@ package effectivejava.chapter7.item47;
 import java.util.stream.Stream;
 import java.util.stream.StreamSupport;
 
-// Adapters from stream to iterable and vice-versa (Page 216)
+// 스트림 <-> 반복자 어댑터 (285-286쪽)
 public class Adapters {
-    // Adapter from  Stream<E> to Iterable<E> (
+    // 코드 47-3 Stream<E>를 Iterable<E>로 중개해주는 어댑터 (285쪽)
     public static <E> Iterable<E> iterableOf(Stream<E> stream) {
         return stream::iterator;
     }
 
-    // Adapter from Iterable<E> to Stream<E>
+    // 코드 47-4 Iterable<E>를 Stream<E>로 중개해주는 어댑터 (286쪽)
     public static <E> Stream<E> streamOf(Iterable<E> iterable) {
         return StreamSupport.stream(iterable.spliterator(), false);
     }
