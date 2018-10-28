d1dc97a5e152e7f3860e2224a56468478c0814c2
Wegra Lee <wegra.lee@gmail.com>
2018-10-28 15:53:33 +0900

    한글화

--- src/effectivejava/chapter10/item74/IndexOutOfBoundsException.java
+++ src/effectivejava/chapter10/item74/IndexOutOfBoundsException.java
@@ -1,25 +1,26 @@
 package effectivejava.chapter10.item74;
 
+// 실패 상황을 온전히 포착하도록 수정해본 IndexOutOfBoundsException (405쪽)
 public class IndexOutOfBoundsException extends RuntimeException {
     private final int lowerBound;
     private final int upperBound;
     private final int index;
 
     /**
-     * Constructs an IndexOutOfBoundsException.
+     * IndexOutOfBoundsException을 생성한다.
      *
-     * @param lowerBound the lowest legal index value
-     * @param upperBound the highest legal index value plus one
-     * @param index      the actual index value
+     * @param lowerBound 인덱스의 최솟값
+     * @param upperBound 인덱스의 최댓값 + 1
+     * @param index      인덱스의 실젯값
      */
     public IndexOutOfBoundsException(int lowerBound, int upperBound,
                                      int index) {
-        // Generate a detail message that captures the failure
+        // 실패를 포착하는 상세 메시지를 생성한다.
         super(String.format(
-                "Lower bound: %d, Upper bound: %d, Index: %d",
+                "최솟값: %d, 최댓값: %d, 인덱스: %d",
                 lowerBound, upperBound, index));
 
-        // Save failure information for programmatic access
+        // 프로그램에서 이용할 수 있도록 실패 정보를 저장해둔다.
         this.lowerBound = lowerBound;
         this.upperBound = upperBound;
         this.index = index;
