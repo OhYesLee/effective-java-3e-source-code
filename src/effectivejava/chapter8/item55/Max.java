d7bece36e229c9b5ab12c626d9f754324fe0fe66
Wegra Lee <wegra.lee@gmail.com>
2018-10-28 11:57:44 +0900

    한글화

--- src/effectivejava/chapter8/item55/Max.java
+++ src/effectivejava/chapter8/item55/Max.java
@@ -2,12 +2,12 @@ package effectivejava.chapter8.item55;
 
 import java.util.*;
 
-// Using Optional<T> as a return type (Pages 249-251)
+// 반환 타입으로 Optional<T> 사용하기 (327-328쪽)
 public class Max {
-//    // Returns maximum value in collection - throws exception if empty (Page 249)
+//    // 코드 55-1 컬렉션에서 최댓값을 구한다. - 컬렉션이 비었으면 예외를 던진다. (327쪽)
 //    public static <E extends Comparable<E>> E max(Collection<E> c) {
 //        if (c.isEmpty())
-//            throw new IllegalArgumentException("Empty collection");
+//            throw new IllegalArgumentException("빈 컬렉션");
 //
 //        E result = null;
 //        for (E e : c)
@@ -17,7 +17,7 @@ public class Max {
 //        return result;
 //    }
 
-//    // Returns maximum value in collection as an Optional<E> (Page 250)
+//    // 코드 55-2 컬렉션에서 최댓값을 구해 Optional<E>로 반환한다. (327쪽)
 //    public static <E extends Comparable<E>>
 //    Optional<E> max(Collection<E> c) {
 //        if (c.isEmpty())
@@ -31,7 +31,7 @@ public class Max {
 //        return Optional.of(result);
 //    }
 
-    // Returns max val in collection as Optional<E> - uses stream (Page 250)
+    // 코드 55-3 컬렉션에서 최댓값을 구해 Optional<E>로 반환한다. - 스트림 버전 (328쪽)
     public static <E extends Comparable<E>>
     Optional<E> max(Collection<E> c) {
         return c.stream().max(Comparator.naturalOrder());
@@ -42,8 +42,8 @@ public class Max {
 
         System.out.println(max(words));
 
-        // Using an optional to provide a chosen default value (Page 251)
-        String lastWordInLexicon = max(words).orElse("No words...");
+        // 코드 55-4 옵셔널 활용 1 - 기본값을 정해둘 수 있다. (328쪽)
+        String lastWordInLexicon = max(words).orElse("단어 없음...");
         System.out.println(lastWordInLexicon);
     }
 }
