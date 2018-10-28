f70fa7875c009e817dded589938f2dd9542395f3
Wegra Lee <wegra.lee@gmail.com>
2018-10-28 09:52:30 +0900

    한글화

--- src/effectivejava/chapter6/item39/markerannotation/Test.java
+++ src/effectivejava/chapter6/item39/markerannotation/Test.java
@@ -1,14 +1,12 @@
 package effectivejava.chapter6.item39.markerannotation;
 import java.lang.annotation.*;
 
-// Marker annotation type declaration - Page 180
+// 코드 39-1 마커(marker) 애너테이션 타입 선언 (238쪽)
 import java.lang.annotation.*;
 
-// Marker annotation type declaration (Page 180)
-
 /**
- * Indicates that the annotated method is a test method.
- * Use only on parameterless static methods.
+ * 테스트 메서드임을 선언하는 애너테이션이다.
+ * 매개변수 없는 정적 메서드 전용이다.
  */
 @Retention(RetentionPolicy.RUNTIME)
 @Target(ElementType.METHOD)
