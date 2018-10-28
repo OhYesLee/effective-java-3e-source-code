727896a242414a81533f933f72ec6969ab65a644
Wegra Lee <wegra.lee@gmail.com>
2018-10-28 11:49:17 +0900

    한글화

--- src/effectivejava/chapter8/item52/Overriding.java
+++ src/effectivejava/chapter8/item52/Overriding.java
@@ -2,7 +2,7 @@ package effectivejava.chapter8.item52;
 
 import java.util.List;
 
-// Classification using method overrriding (Page 239)
+// 재정의된 메서드 호출 메커니즘 (313쪽, 코드 52-2의 일부)
 public class Overriding {
     public static void main(String[] args) {
         List<Wine> wineList = List.of(
