0c246d2884dd60fb1f70a8d54aaa3d509b119514
Wegra Lee <wegra.lee@gmail.com>
2018-10-28 15:41:34 +0900

    한글화

--- src/effectivejava/chapter9/item59/RandomBug.java
+++ src/effectivejava/chapter9/item59/RandomBug.java
@@ -1,15 +1,16 @@
 package effectivejava.chapter9.item59;
 import java.util.*;
 
-// Random number generation is hard! - Page 215
+// 무작위 수 생성은 쉽지 않다.
 public class RandomBug {
-    // Common but deeply flawed!
+    // 코드 59-1 흔하지만 문제가 심각한 코드! (351쪽)
     static Random rnd = new Random();
 
     static int random(int n) {
         return Math.abs(rnd.nextInt()) % n;
     }
 
+    // 무작위 수 1백만 개 생성 후, 중간 값보다 작은 수의 개수 출력 (351쪽)
     public static void main(String[] args) {
         int n = 2 * (Integer.MAX_VALUE / 3);
         int low = 0;
