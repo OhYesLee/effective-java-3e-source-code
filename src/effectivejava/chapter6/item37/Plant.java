66f531e94b284518b0ff723a55c7e7d2cb888b86
Wegra Lee <wegra.lee@gmail.com>
2018-10-28 09:29:32 +0900

    한글화

--- src/effectivejava/chapter6/item37/Plant.java
+++ src/effectivejava/chapter6/item37/Plant.java
@@ -3,9 +3,9 @@ import java.util.*;
 import static java.util.stream.Collectors.groupingBy;
 import static java.util.stream.Collectors.toSet;
 
-// Using an EnumMap to associate data with an enum (Pages 171-3)
+// EnumMap을 사용해 열거 타입에 데이터를 연관시키기 (226-228쪽)
 
-// Simplistic class representing a plant (Page 171)
+// 식물을 아주 단순하게 표현한 클래스 (226쪽)
 class Plant {
     enum LifeCycle { ANNUAL, PERENNIAL, BIENNIAL }
 
@@ -23,28 +23,28 @@ class Plant {
 
     public static void main(String[] args) {
         Plant[] garden = {
-            new Plant("Basil",    LifeCycle.ANNUAL),
-            new Plant("Carroway", LifeCycle.BIENNIAL),
-            new Plant("Dill",     LifeCycle.ANNUAL),
-            new Plant("Lavendar", LifeCycle.PERENNIAL),
-            new Plant("Parsley",  LifeCycle.BIENNIAL),
-            new Plant("Rosemary", LifeCycle.PERENNIAL)
+            new Plant("바질",    LifeCycle.ANNUAL),
+            new Plant("캐러웨이", LifeCycle.BIENNIAL),
+            new Plant("딜",      LifeCycle.ANNUAL),
+            new Plant("라벤더",   LifeCycle.PERENNIAL),
+            new Plant("파슬리",   LifeCycle.BIENNIAL),
+            new Plant("로즈마리", LifeCycle.PERENNIAL)
         };
 
-        // Using ordinal() to index into an array - DON'T DO THIS!  (Page 171)
+        // 코드 37-1 ordinal()을 배열 인덱스로 사용 - 따라 하지 말 것! (226쪽)
         Set<Plant>[] plantsByLifeCycleArr =
                 (Set<Plant>[]) new Set[Plant.LifeCycle.values().length];
         for (int i = 0; i < plantsByLifeCycleArr.length; i++)
             plantsByLifeCycleArr[i] = new HashSet<>();
         for (Plant p : garden)
             plantsByLifeCycleArr[p.lifeCycle.ordinal()].add(p);
-        // Print the results
+        // 결과 출력
         for (int i = 0; i < plantsByLifeCycleArr.length; i++) {
             System.out.printf("%s: %s%n",
                     Plant.LifeCycle.values()[i], plantsByLifeCycleArr[i]);
         }
 
-        // Using an EnumMap to associate data with an enum (Page 172)
+        // 코드 37-2 EnumMap을 사용해 데이터와 열거 타입을 매핑한다. (227쪽)
         Map<Plant.LifeCycle, Set<Plant>> plantsByLifeCycle =
                 new EnumMap<>(Plant.LifeCycle.class);
         for (Plant.LifeCycle lc : Plant.LifeCycle.values())
@@ -53,11 +53,11 @@ class Plant {
             plantsByLifeCycle.get(p.lifeCycle).add(p);
         System.out.println(plantsByLifeCycle);
 
-        // Naive stream-based approach - unlikely to produce an EnumMap!  (Page 172)
+        // 코드 37-3 스트림을 사용한 코드 1 - EnumMap을 사용하지 않는다! (228쪽)
         System.out.println(Arrays.stream(garden)
                 .collect(groupingBy(p -> p.lifeCycle)));
 
-        // Using a stream and an EnumMap to associate data with an enum (Page 173)
+        // 코드 37-4 스트림을 사용한 코드 2 - EnumMap을 이용해 데이터와 열거 타입을 매핑했다. (228쪽)
         System.out.println(Arrays.stream(garden)
                 .collect(groupingBy(p -> p.lifeCycle,
                         () -> new EnumMap<>(LifeCycle.class), toSet())));
