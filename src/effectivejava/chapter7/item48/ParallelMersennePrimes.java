6d333f0d6a6f295ffcab691e631e19dca62110d0
Wegra Lee <wegra.lee@gmail.com>
2018-10-28 11:36:16 +0900

    한글화

--- src/effectivejava/chapter7/item48/ParallelMersennePrimes.java
+++ src/effectivejava/chapter7/item48/ParallelMersennePrimes.java
@@ -5,7 +5,7 @@ import java.util.stream.Stream;
 
 import static java.math.BigInteger.*;
 
-// 병렬 스트림을 사용해 처음 20개의 메르센 소수를 생성하는 프로그램 (291쪽의 코드 48-1을 병렬화했다.)
+// 병렬 스트림을 사용해 처음 20개의 메르센 소수를 생성하는 프로그램 (291쪽 코드 48-1의 병렬화 버전)
 // 주의: 병렬화의 영향으로 프로그램이 종료하지 않는다.
 public class ParallelMersennePrimes {
     public static void main(String[] args) {
