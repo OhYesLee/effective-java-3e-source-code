2e8cb0a796bec994d8450b353259058abd1f21be
Wegra Lee <wegra.lee@gmail.com>
2018-10-28 09:55:44 +0900

    한글화

--- src/effectivejava/chapter6/item39/annotationwithparameter/ExceptionTest.java
+++ src/effectivejava/chapter6/item39/annotationwithparameter/ExceptionTest.java
@@ -1,10 +1,10 @@
 package effectivejava.chapter6.item39.annotationwithparameter;
 
-// Annotation type with a parameter (Page 183)
+// 코드 39-4 매개변수 하나를 받는 애너테이션 타입 (240-241쪽)
 import java.lang.annotation.*;
+
 /**
- * Indicates that the annotated method is a test method that
- * must throw the designated exception to succeed.
+ * 명시한 예외를 던져야만 성공하는 테스트 메서드용 애너테이션
  */
 @Retention(RetentionPolicy.RUNTIME)
 @Target(ElementType.METHOD)
