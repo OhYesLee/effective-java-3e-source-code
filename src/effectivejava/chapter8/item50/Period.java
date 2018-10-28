20177204f3cb940cf7813de41af5e18b95881ca5
Wegra Lee <wegra.lee@gmail.com>
2018-10-28 11:41:31 +0900

    한글화

--- src/effectivejava/chapter8/item50/Period.java
+++ src/effectivejava/chapter8/item50/Period.java
@@ -1,21 +1,21 @@
 package effectivejava.chapter8.item50;
 import java.util.*;
 
-// Broken "immutable" time period class (Pages 231-3)
+// 코드 50-1 기간을 표현하는 클래스 - 불변식을 지키지 못했다. (302-305쪽)
 public final class Period {
     private final Date start;
     private final Date end;
 
     /**
-     * @param  start the beginning of the period
-     * @param  end the end of the period; must not precede start
-     * @throws IllegalArgumentException if start is after end
-     * @throws NullPointerException if start or end is null
+     * @param  start 시작 시각
+     * @param  end 종료 시각. 시작 시각보다 뒤여야 한다.
+     * @throws IllegalArgumentException 시작 시각이 종료 시각보다 늦을 때 발생한다.
+     * @throws NullPointerException start나 end가 null이면 발생한다.
      */
     public Period(Date start, Date end) {
         if (start.compareTo(end) > 0)
             throw new IllegalArgumentException(
-                    start + " after " + end);
+                    start + "가 " + end + "보다 늦다.");
         this.start = start;
         this.end   = end;
     }
@@ -31,17 +31,17 @@ public final class Period {
         return start + " - " + end;
     }
 
-//    // Repaired constructor - makes defensive copies of parameters (Page 232)
+//    // 코드 50-3 수정한 생성자 - 매개변수의 방어적 복사본을 만든다. (304쪽)
 //    public Period(Date start, Date end) {
 //        this.start = new Date(start.getTime());
 //        this.end   = new Date(end.getTime());
 //
 //        if (this.start.compareTo(this.end) > 0)
 //            throw new IllegalArgumentException(
-//                    this.start + " after " + this.end);
+//                    this.start + "가 " + this.end + "보다 늦다.");
 //    }
 //
-//    // Repaired accessors - make defensive copies of internal fields (Page 233)
+//    // 코드 50-5 수정한 접근자 - 필드의 방어적 복사본을 반환한다. (305쪽)
 //    public Date start() {
 //        return new Date(start.getTime());
 //    }
@@ -50,5 +50,5 @@ public final class Period {
 //        return new Date(end.getTime());
 //    }
 
-    // Remainder omitted
+    // 나머지 코드 생략
 }
