78b77c85b52544dc8cbae6d037450aae302b23ac
Wegra Lee <wegra.lee@gmail.com>
2018-10-28 10:25:41 +0900

    한글화

--- src/effectivejava/chapter7/item42/SortFourWays.java
+++ src/effectivejava/chapter7/item42/SortFourWays.java
@@ -8,12 +8,12 @@ import static java.util.Comparator.*;
 
 import static java.util.Comparator.comparingInt;
 
-// Sorting with function objects (Pages 193-4)
+// 함수 객체로 정렬하기 (254-255쪽)
 public class SortFourWays {
     public static void main(String[] args) {
         List<String> words = Arrays.asList(args);
 
-        // Anonymous class instance as a function object - obsolete! (Page 193)
+        // 코드 42-1 익명 클래스의 인스턴스를 함수 객체로 사용 - 낡은 기법이다! (254쪽)
         Collections.sort(words, new Comparator<String>() {
             public int compare(String s1, String s2) {
                 return Integer.compare(s1.length(), s2.length());
@@ -22,18 +22,18 @@ public class SortFourWays {
         System.out.println(words);
         Collections.shuffle(words);
 
-        // Lambda expression as function object (replaces anonymous class) (Page 194)
+        // 코드 42-2 람다식을 함수 객체로 사용 - 익명 클래스 대체 (255쪽)
         Collections.sort(words,
                 (s1, s2) -> Integer.compare(s1.length(), s2.length()));
         System.out.println(words);
         Collections.shuffle(words);
 
-        // Comparator construction method (with method reference) in place of lambda (Page 194)
+        // 람다 자리에 비교자 생성 메서드(메서드 참조와 함께)를 사용 (255쪽)
         Collections.sort(words, comparingInt(String::length));
         System.out.println(words);
         Collections.shuffle(words);
 
-        // Default method List.sort in conjunction with comparator construction method (Page 194)
+        // 비교자 생성 메서드와 List.sort를 사용 (255쪽)
         words.sort(comparingInt(String::length));
         System.out.println(words);
     }
