2fd71af7cf043cc2dde0f0b654402234c901eee2
Wegra Lee <wegra.lee@gmail.com>
2018-10-28 12:03:50 +0900

    한글화

--- src/effectivejava/chapter8/item55/ParentPid.java
+++ src/effectivejava/chapter8/item55/ParentPid.java
@@ -2,18 +2,18 @@ package effectivejava.chapter8.item55;
 
 import java.util.Optional;
 
-// Avoiding unnecessary use of Optional's isPresent method (Page 252)
+// 불필요하게 사용한 Optional의 isPresent 메서드를 제거하자. (329쪽)
 public class ParentPid {
     public static void main(String[] args) {
         ProcessHandle ph = ProcessHandle.current();
 
-        // Inappropriate use of isPresent
+        // isPresent를 적절치 못하게 사용했다.
         Optional<ProcessHandle> parentProcess = ph.parent();
-        System.out.println("Parent PID: " + (parentProcess.isPresent() ?
+        System.out.println("부모 PID: " + (parentProcess.isPresent() ?
                 String.valueOf(parentProcess.get().pid()) : "N/A"));
 
-        // Equivalent (and superior) code using orElse
-        System.out.println("Parent PID: " +
+        // 같은 기능을 Optional의 map를 이용해 개선한 코드
+        System.out.println("부모 PID: " +
             ph.parent().map(h -> String.valueOf(h.pid())).orElse("N/A"));
     }
 }
