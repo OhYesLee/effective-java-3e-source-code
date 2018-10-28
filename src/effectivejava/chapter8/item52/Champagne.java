cd2e80c7bd1acbc28a9b239b998eb92179bef06e
Wegra Lee <wegra.lee@gmail.com>
2018-10-28 11:48:01 +0900

    한글화

--- src/effectivejava/chapter8/item52/Champagne.java
+++ src/effectivejava/chapter8/item52/Champagne.java
@@ -1,6 +1,6 @@
 package effectivejava.chapter8.item52;
 
-// Classification using method overrriding (Page 239)
+// 메서드 재정의를 이용한 분류 (313쪽, 코드 52-2의 일부)
 class Champagne extends SparklingWine {
-    @Override String name() { return "champagne"; }
+    @Override String name() { return "샴페인"; }
 }
