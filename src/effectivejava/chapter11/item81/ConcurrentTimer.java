6577734f0a6098e050f9f067e480417e56408f40
Wegra Lee <wegra.lee@gmail.com>
2018-10-28 16:14:56 +0900

    한글화

--- src/effectivejava/chapter11/item81/ConcurrentTimer.java
+++ src/effectivejava/chapter11/item81/ConcurrentTimer.java
@@ -1,9 +1,9 @@
 package effectivejava.chapter11.item81;
 import java.util.concurrent.*;
 
-// Simple framework for timing concurrent execution 327
+// 코드 81-3 동시 실행 시간을 재는 간단한 프레임워크 (433-434쪽)
 public class ConcurrentTimer {
-    private ConcurrentTimer() { } // Noninstantiable
+    private ConcurrentTimer() { } // 인스턴스 생성 불가
 
     public static long time(Executor executor, int concurrency,
                             Runnable action) throws InterruptedException {
@@ -13,22 +13,22 @@ public class ConcurrentTimer {
 
         for (int i = 0; i < concurrency; i++) {
             executor.execute(() -> {
-                ready.countDown(); // Tell timer we're ready
+                ready.countDown(); // 타이머에게 준비를 마쳤음을 알린다.
                 try {
-                    start.await(); // Wait till peers are ready
+                    start.await(); // 모든 작업자 스레드가 준비될 때까지 기다린다.
                     action.run();
                 } catch (InterruptedException e) {
                     Thread.currentThread().interrupt();
                 } finally {
-                    done.countDown();  // Tell timer we're done
+                    done.countDown();  // 타이머에게 작업을 마쳤음을 알린다.
                 }
             });
         }
 
-        ready.await();     // Wait for all workers to be ready
+        ready.await();     // 모든 작업자가 준비될 때까지 기다린다.
         long startNanos = System.nanoTime();
-        start.countDown(); // And they're off!
-        done.await();      // Wait for all workers to finish
+        start.countDown(); // 작업자들을 깨운다.
+        done.await();      // 모든 작업자가 일을 끝마치기를 기다린다.
         return System.nanoTime() - startNanos;
     }
 }
