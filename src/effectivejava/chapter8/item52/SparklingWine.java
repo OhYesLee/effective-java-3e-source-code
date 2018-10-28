a0dddbd716952c041bcbafdf80068cb2a37eb449
Wegra Lee <wegra.lee@gmail.com>
2018-10-28 11:47:13 +0900

    한글화

--- src/effectivejava/chapter8/item52/SparklingWine.java
+++ src/effectivejava/chapter8/item52/SparklingWine.java
@@ -1,6 +1,6 @@
 package effectivejava.chapter8.item52;
 
-// 메서드 재정의를 이용한 분류 (313쪽)
+// 메서드 재정의를 이용한 분류 (313쪽, 코드 52-2의 일부)
 class SparklingWine extends Wine {
     @Override String name() { return "발포성 포도주"; }
 }
