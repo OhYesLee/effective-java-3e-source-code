89bf072b8ab4370adbe16028bb3f35bb32929563
Wegra Lee <wegra.lee@gmail.com>
2018-10-28 09:35:49 +0900

    한글화

--- src/effectivejava/chapter6/item38/Operation.java
+++ src/effectivejava/chapter6/item38/Operation.java
@@ -1,6 +1,6 @@
 package effectivejava.chapter6.item38;
 
-// Emulated extensible enum using an interface (Page 176)
+// 코드 38-1 인터페이스를 이용해 확장 가능 열거 타입을 흉내 냈다. (232쪽)
 public interface Operation {
     double apply(double x, double y);
 }
