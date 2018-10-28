8386419f13834ad987af1e1f67969547b9186efd
Wegra Lee <wegra.lee@gmail.com>
2018-10-28 11:45:17 +0900

    한글화

--- src/effectivejava/chapter8/item52/CollectionClassifier.java
+++ src/effectivejava/chapter8/item52/CollectionClassifier.java
@@ -2,18 +2,18 @@ package effectivejava.chapter8.item52;
 import java.util.*;
 import java.math.*;
 
-// Broken! - What does this program print?  (Page 238)
+// 코드 52-1 컬렉션 분류기 - 오류! 이 프로그램은 무엇을 출력할까? (312쪽)
 public class CollectionClassifier {
     public static String classify(Set<?> s) {
-        return "Set";
+        return "집합(Set)";
     }
 
     public static String classify(List<?> lst) {
-        return "List";
+        return "리스트(List)";
     }
 
     public static String classify(Collection<?> c) {
-        return "Unknown Collection";
+        return "그 외 컬렉션";
     }
 
     public static void main(String[] args) {
