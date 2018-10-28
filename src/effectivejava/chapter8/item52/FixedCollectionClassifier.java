b2c7e47d2e3f52b09647302075f6e3857011206e
Wegra Lee <wegra.lee@gmail.com>
2018-10-28 11:53:00 +0900

    한글화

--- src/effectivejava/chapter8/item52/FixedCollectionClassifier.java
+++ src/effectivejava/chapter8/item52/FixedCollectionClassifier.java
@@ -3,11 +3,11 @@ package effectivejava.chapter8.item52;
 import java.math.BigInteger;
 import java.util.*;
 
-// Repaired  static classifier method. (Page 240)
+// 수정된 컬렉션 분류기 (314쪽)
 public class FixedCollectionClassifier {
     public static String classify(Collection<?> c) {
-        return c instanceof Set  ? "Set" :
-                c instanceof List ? "List" : "Unknown Collection";
+        return c instanceof Set  ? "집합" :
+                c instanceof List ? "리스트" : "그 외";
     }
 
     public static void main(String[] args) {
