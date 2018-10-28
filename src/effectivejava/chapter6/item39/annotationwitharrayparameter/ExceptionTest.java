59c8ea67b86733780321242cb8f1f0b4709bb717
Wegra Lee <wegra.lee@gmail.com>
2018-10-28 10:00:06 +0900

    한글화

--- src/effectivejava/chapter6/item39/annotationwitharrayparameter/ExceptionTest.java
+++ src/effectivejava/chapter6/item39/annotationwitharrayparameter/ExceptionTest.java
@@ -1,7 +1,7 @@
 package effectivejava.chapter6.item39.annotationwitharrayparameter;
 import java.lang.annotation.*;
 
-// Annotation type with an array parameter  (Page 184)
+// 코드 39-6 배열 매개변수를 받는 애너테이션 타입 (242쪽)
 @Retention(RetentionPolicy.RUNTIME)
 @Target(ElementType.METHOD)
 public @interface ExceptionTest {
