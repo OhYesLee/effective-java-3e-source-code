3c818b61a06aba8e146a1b6f781930277649d6dc
Wegra Lee <wegra.lee@gmail.com>
2018-10-28 10:34:45 +0900

    한글화

--- src/effectivejava/chapter7/item45/CharStream.java
+++ src/effectivejava/chapter7/item45/CharStream.java
@@ -1,13 +1,13 @@
 package effectivejava.chapter7.item45;
 
-// Refrain from using streams to process char values (Page 206)
+// char 데이터를 처리할 때는 스트림 사용을 자제하자. (272쪽)
 public class CharStream {
     public static void main(String[] args) {
-        // Does not produce the expected result
+        // 예상한 결과와 다르다.
         "Hello world!".chars().forEach(System.out::print);
         System.out.println();
 
-        // Fixes the problem
+        // 문제를 고치려면 형변환을 명시적으로 해줘야 한다.
         "Hello world!".chars().forEach(x -> System.out.print((char) x));
         System.out.println();
     }
