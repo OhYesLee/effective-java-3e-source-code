9c4de52c8d41f5cf15661cac94171ced5db59057
Wegra Lee <wegra.lee@gmail.com>
2018-10-28 11:43:23 +0900

    한글화

--- src/effectivejava/chapter8/item50/Attacks.java
+++ src/effectivejava/chapter8/item50/Attacks.java
@@ -1,21 +1,21 @@
 package effectivejava.chapter8.item50;
 import java.util.*;
 
-// Two attacks on the internals of an "immutable" period (232-3)
+// '불변'인 Period의 내부를 공격하는 두 가지 예 (303-305쪽)
 public class Attacks {
     public static void main(String[] args) {
-        // Attack the internals of a Period instance  (Page 232)
+        // 코드 50-2 Period 인스턴스의 내부를 공격해보자. (303쪽)
         Date start = new Date();
         Date end = new Date();
         Period p = new Period(start, end);
-        end.setYear(78);  // Modifies internals of p!
+        end.setYear(78);  // p의 내부를 변경했다!
         System.out.println(p);
 
-        // Second attack on the internals of a Period instance  (Page 233)
+        // 코드 50-4 Period 인스턴스를 향한 두 번째 공격 (305쪽)
         start = new Date();
         end = new Date();
         p = new Period(start, end);
-        p.end().setYear(78);  // Modifies internals of p!
+        p.end().setYear(78);  // p의 내부를 변경했다!
         System.out.println(p);
     }
 }
