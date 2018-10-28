b0d2181c3136d9fd6ecab1c84b94b6cff25b58fe
Wegra Lee <wegra.lee@gmail.com>
2018-10-28 12:40:11 +0900

    한글화

--- src/effectivejava/chapter9/item58/DiceRolls.java
+++ src/effectivejava/chapter9/item58/DiceRolls.java
@@ -1,12 +1,12 @@
 package effectivejava.chapter9.item58;
 import java.util.*;
 
-// Same bug as NestIteration.java (but different symptom)!! - Page 213
+// 코드 58-5 같은 버그, 다른 증상! (349쪽)
 public class DiceRolls {
     enum Face { ONE, TWO, THREE, FOUR, FIVE, SIX }
 
     public static void main(String[] args) {
-        // Same bug, different symptom!
+        // 같은 버그, 다른 증상!
         Collection<Face> faces = EnumSet.allOf(Face.class);
 
         for (Iterator<Face> i = faces.iterator(); i.hasNext(); )
@@ -15,6 +15,7 @@ public class DiceRolls {
 
         System.out.println("***************************");
 
+        // 컬렉션이나 배열의 중첩 반복을 위한 권장 관용구
         for (Face f1 : faces)
             for (Face f2 : faces)
                 System.out.println(f1 + " " + f2);
