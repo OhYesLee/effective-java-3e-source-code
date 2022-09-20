9f7812f3246ebb052badcdbed96d19e2a72dea48
sung_uk <103974953+OhYesLee@users.noreply.github.com>
2022-09-20 13:50:38 +0900

    Update Sum.java

--- src/effectivejava/chapter2/item6/Sum.java
+++ src/effectivejava/chapter2/item6/Sum.java
@@ -5,7 +5,7 @@ import java.util.Comparator;
 // 코드 6-3 끔찍이 느리다! 객체가 만들어지는 위치를 찾았는가? (34쪽)
 public class Sum {
     private static long sum() {
-        Long sum = 0L;
+        long sum = 0L;
         for (long i = 0; i <= Integer.MAX_VALUE; i++)
             sum += i;
         return sum;
